1862ffaf690101ac258af965b8b6b283f61da739
Rishi Aditya Rai <rishiaditya19@gmail.com>
2026-01-03 17:00:27 +0530

    Phase 0.3 completed

---
+++ backend/src/main/java/com/cinemahub/backend/CinemahubApplication.java
@@ -0,0 +1,13 @@
+package com.cinemahub.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemahubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemahubApplication.class, args);
+	}
+
+}
