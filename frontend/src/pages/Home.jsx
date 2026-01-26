2220bf66199df678d8e5776a9fc424a8d07cca3f
Rishi Aditya Rai <rishiaditya19@gmail.com>
2026-01-27 02:10:28 +0530

    Front end setup with react routing

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+            <h2>Welcome to Cinemahun</h2>
+            <p>Select a movie to continue</p>
+        </div>
+    );
+}
+
+export default Home;
