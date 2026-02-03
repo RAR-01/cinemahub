4f2933f18a94533171800accd44624bb22f0630c
Rishi Aditya Rai <rishiaditya19@gmail.com>
2026-02-03 23:31:27 +0530

    Changes in moviedetails at frontend

--- frontend/src/pages/MovieDetails.jsx
+++ frontend/src/pages/MovieDetails.jsx
@@ -1,5 +1,36 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getMovieById } from "../services/movieService";
+
 function MovieDetails() {
-  return <h1>Movie Details</h1>;
+  const { movieId } = useParams();
+  const [movie, setMovie] = useParams(null);
+
+  useEffect(() => {
+    const fetchMovie = async () => {
+      try {
+        const response = await getMovieById(movieId);
+        setMovie(response.data);
+      } catch (error) {
+        console.error("Error fetching movie details", error);
+      }
+    };
+
+    fetchMovie();
+  }, [movieId]);
+
+  if (!movie) {
+    return <h2>Loading...</h2>;
+  }
+
+  return(
+    <div>
+      <h1>{movie.title}</h1>
+      <p><strong>Genre:</strong> {movie.genre}</p>
+      <p><strong>Rating:</strong> {movie.rating}</p>
+    </div>
+  );
 }
+
 
 export default MovieDetails;
